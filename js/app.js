b731279a25ecb7e1aac2476a60d16b2bb28d1e95
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-10-29 16:52:52 -0700

    added base functionality before appending images to DOM

--- js/app.js
+++ js/app.js
@@ -7,3 +7,33 @@ function Image(img) {
   this.keyword = img.keyword;
   this.horns = img.horns;
 }
+
+Image.allImages = [];
+
+Image.prototype.render = function() {
+  let imageClone = $('#photo-template').clone();
+  let $imageClone = $(imageClone[0].content);
+
+  $imageClone.find('h2').text(this.title);
+  $imageClone.find('img').attr('src', this.image_url);
+  $imageClone.find('p').text(this.hobbies);
+  $imageClone.attr('class', this.title);
+
+  $imageClone.appendTo('main');
+}
+
+Image.getJson = () => {
+  $.get('../data/page-1.json')
+    .then(data => {
+      data.forEach(item => {
+        Image.allImages.push(new Image(item))
+      });
+    })
+    .then(Image.loadImages);
+};
+
+Image.loadImages = () => {
+  Image.allImages.forEach(image => image.render());
+}
+
+$(() => Image.getJson());
