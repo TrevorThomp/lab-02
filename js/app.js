34b8f83d00918246d2802c82651b4caa19695336
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-10-29 16:34:13 -0700

    completed constructor function

---
+++ js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function Image(img) {
+  this.image_url = img.image_url;
+  this.title = img.title;
+  this.description = img.description;
+  this.keyword = img.keyword;
+  this.horns = img.horns;
+}
